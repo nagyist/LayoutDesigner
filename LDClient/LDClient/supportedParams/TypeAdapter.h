42869bbb168e812d07ec50103881c65c72217e07
Ved Surtani <ved.redjack@gmail.com>
2011-11-08 14:44:29 +0530

    remove messageParams and add Adapters instead.

---
+++ LDClient/LDClient/supportedParams/TypeAdapter.h
@@ -0,0 +1,13 @@
+//
+//  TypeAdapter.h
+//  LDClient
+//
+//  Created by Ved Surtani on 04/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol TypeAdapter <NSObject>
+
+@end
