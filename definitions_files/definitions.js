c12ada57306271c5e00da783226cf32ce80d3b46
Ved Surtani <ved.redjack@gmail.com>
2011-11-17 18:18:42 +0530

    initial commit

---
+++ definitions_files/definitions.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20111117
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('definitions_files/definitionsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ ldserver_files/ldserver.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20111117
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('ldserver_files/ldserverMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
